626931f17e99f68c54c79d891288557674843632
Joakim Larsson <mimmertim@gmail.com>
2024-11-25 17:34:14 +0100

    tidy

--- malaria.py
+++ malaria.py
@@ -45,3 +45,5 @@ if __name__ == "__main__":
 
 # uninfected
 # python malaria.py --src 'https://storage.googleapis.com/kagglesdsdata/datasets/87153/200743/cell_images/Uninfected/C100P61ThinF_IMG_20150918_144104_cell_128.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=databundle-worker-v2%40kaggle-161607.iam.gserviceaccount.com%2F20241125%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20241125T154810Z&X-Goog-Expires=345600&X-Goog-SignedHeaders=host&X-Goog-Signature=264457827c4259123c6ca34e5e567fc95f8cdc434cfe7c5e4e613e1345216c4c7dd965c83fbfae7a2b937465d3d4cf876be0e6d81215c0238314bee21d9ecf0499397e28faf0784b90f17ddfed517f6b0fda08f505a0c1c431dd8e455f54359bdd5d0cc2d9a52ef138602e5b4eda80bac7a5884d6b98ecc9d9f556311cba319061317a5464c3fb9513167fdbf34e529361ff7d341ac1e94ee200143e9d4b149c06558ee6d03f14ff88510c708965824d49afb829a7f7ea62c3e2c6eff75dbe41b2ba5fd8849ad87e64540da6511b2d3441671a015368299fad2ee62570afe144c7cc2f6e8f4f1cc534a7b048633d4be8f8e88a5aaf1662bb15e9680d199b490f'
+
+# use additional images from https://data.lhncbc.nlm.nih.gov/public/Malaria/cell_images.zip
